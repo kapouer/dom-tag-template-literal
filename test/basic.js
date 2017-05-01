a61b746518999f33c3d1e95aeae200a0208573d1
Jérémy Lal <kapouer@melix.org>
2017-05-01 23:13:35 +0200

    Renaming

--- test/basic.js
+++ test/basic.js
@@ -1,16 +1,16 @@
 const assert = require('assert')
-const domify = require('../src/')
+const dom = require('../src/')
 
-describe('domify tagged template string', function() {
+describe('dom tagged template string', function() {
   it('should return a DOM node', function() {
-    let node = domify`<span></span>`
+    let node = dom`<span></span>`
     assert.ok(node instanceof Node)
     assert.equal(node.tagName, "SPAN")
     assert.equal(node.ownerDocument, document)
   })
 
   it('should return a DOM fragment', function() {
-    let node = domify`<tr><td>a</td></tr><tr><td>b</td></tr>`
+    let node = dom`<tr><td>a</td></tr><tr><td>b</td></tr>`
     assert.ok(node instanceof Node)
     assert.equal(node.querySelectorAll('tr').length, 2)
     assert.equal(node.querySelectorAll('tr > td').length, 2)
@@ -23,7 +23,7 @@ describe('domify tagged template string', function() {
       style: 'color:red',
       text: 'awesome'
     }
-    let node = domify`<span style="${obj.style}">${obj.text}</span>`
+    let node = dom`<span style="${obj.style}">${obj.text}</span>`
     assert.equal(node.innerHTML, obj.text)
     assert.equal(node.attributes.style.value, 'color:red')
     assert.equal(node.style.color, 'red')
@@ -31,9 +31,9 @@ describe('domify tagged template string', function() {
 
   it('should replace a Node value', function() {
     const items = [ 'One', 'Two', 'Three' ]
-    const list = domify `<ul>${items.map(label => {
-      const removeBtn = domify `<button>X</button>`
-      const node = domify `<li>${label} ${removeBtn}</li>`
+    const list = dom`<ul>${items.map(label => {
+      const removeBtn = dom`<button>X</button>`
+      const node = dom`<li>${label} ${removeBtn}</li>`
     return node
     })}</ul>`
     assert.equal(list.querySelectorAll('li').length, 3)
