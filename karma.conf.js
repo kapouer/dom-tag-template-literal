c3c46800a73aef62dacbf4b342b284cf261cd56e
Jérémy Lal <kapouer@melix.org>
2017-05-02 12:59:28 +0200

    And now run on more browsers

--- karma.conf.js
+++ karma.conf.js
@@ -4,13 +4,13 @@ var customLaunchers = {
   // You can use this tool to generate the correct config:
   // https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
   win10chrome: { base: "SauceLabs", browserName: "chrome", platform: "Windows 10" },
-  // androidChrome: { base: "SauceLabs", browserName: "android", platform: "Linux" },
-  // win10firefox: { base: "SauceLabs", browserName: "firefox", platform: "Windows 10" },
-  // iosSafari: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10" },
-  // iosSafari92: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10", version: "9.2" },
-  // win10ie11: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 10" },
-  // win7ie9: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 7", version: "9.0" },
-  // win7ie10: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 7", version: "10.0" }
+  androidChrome: { base: "SauceLabs", browserName: "android", platform: "Linux" },
+  win10firefox: { base: "SauceLabs", browserName: "firefox", platform: "Windows 10" },
+  iosSafari: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10" },
+  iosSafari92: { base: "SauceLabs", browserName: "iphone", platform: "OS X 10.10", version: "9.2" },
+  win10ie11: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 10" },
+  win7ie9: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 7", version: "9.0" },
+  win7ie10: { base: "SauceLabs", browserName: "internet explorer", platform: "Windows 7", version: "10.0" }
 };
 
 module.exports = function(config) {
