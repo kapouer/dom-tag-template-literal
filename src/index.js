ca71f589f9c60f8cba2358461842f0f642194609
Jérémy Lal <kapouer@melix.org>
2017-07-31 17:55:51 +0200

    Implement support for DocumentFragment and NodeList

--- src/index.js
+++ src/index.js
@@ -24,19 +24,24 @@ function generateId () {
 function generateNodes (doc, ...partials) {
   // Array of placeholder IDs
   const placeholders = []
-
   // Generate regular HTML string first
-  const html = partials.reduce((carry, partial) => {
+  function reducer(carry, partial) {
+    if (partial && partial.nodeType == Node.DOCUMENT_FRAGMENT_NODE) {
+      partial = partial.childNodes
+    }
     if (Array.isArray(partial)) {
       carry.concat(partial)
     } else if (typeof partial === 'object' && partial instanceof Node) {
       const id = generateId()
       placeholders.push({ id, node: partial })
       return carry.concat(`<${partial.nodeName} id="${id}"></${partial.nodeName}>`)
+    } else if (partial && typeof partial.item == "function" && typeof partial.length == "number") {
+      return carry.concat(Array.prototype.reduce.call(partial, reducer, []))
     } else {
       return carry.concat(partial)
     }
-  }, []).join('')
+  }
+  const html = partials.reduce(reducer, []).join('')
 
   // Wrap in temporary container node
   let template = doc.createElement('template')
